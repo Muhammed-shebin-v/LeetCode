81fb7cf723f7d50db14510a0969b6fcee5de7825
Muhammed Shebin .v <vakkayilshebin07@gmail.com>
2024-12-09 21:28:12 +0530

    Time: 10 ms (95.45%), Space: 149.9 MB (53.23%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.dart
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.dart
@@ -1,16 +1,14 @@
 class Solution {
   int firstUniqChar(String s) {
     for(int i = 0;i<s.length;i++){
-        bool f =true;
+        bool f = true;
         for(int j=0;j<s.length;j++){
             if(s[i]==s[j] && i!=j){
                 f= false;
                 break;
             }
         }
-        if(f == true){
-            return i;
-        }
+        if(f == true)return i;
     }
     return -1;
   }
