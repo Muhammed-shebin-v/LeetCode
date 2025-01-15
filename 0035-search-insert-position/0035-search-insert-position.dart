f806376aede77475fb39326b45b7a7834f0d98f8
Muhammed Shebin .v <vakkayilshebin07@gmail.com>
2025-01-15 09:42:28 +0530

    Time: 0 ms (100%), Space: 148.4 MB (66.04%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.dart
@@ -0,0 +1,23 @@
+class Solution {
+  int? searchInsert(List<int> nums, int target) {
+    int startidx=0;
+    int endidx = nums.length-1;
+    int middle=0;
+    while(startidx <= endidx){
+       middle=startidx+(endidx-startidx)~/2;
+      if(nums[middle]==target){
+        return middle;
+      }
+      else if(nums[middle]>target){
+        endidx=middle-1;
+      }
+      else{
+        startidx=middle+1;
+      }
+    }
+    if(nums[middle]>target){
+        return middle;
+    }
+    return middle+1;
+  }
+  }
