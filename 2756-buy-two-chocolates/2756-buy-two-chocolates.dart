9b38f97d110865abe92335f8c694464debf67e3d
Muhammed Shebin .v <vakkayilshebin07@gmail.com>
2025-01-16 23:33:12 +0530

    Time: 2 ms (100%), Space: 152.8 MB (12.5%) - LeetHub

--- 2756-buy-two-chocolates/2756-buy-two-chocolates.dart
+++ 2756-buy-two-chocolates/2756-buy-two-chocolates.dart
@@ -4,14 +4,12 @@ class Solution {
     bool flag=false;
     for(int i=0;i<prices.length;i++){
         for(int j=0;j<prices.length;j++){
-            if(i!=j){
-                if(prices[i]+prices[j]<=money){
+                if(i!=j&&prices[i]+prices[j]<=money && sumprice<=money-(prices[i]+prices[j])){
                     flag=true;
-                    if(sumprice<money-(prices[i]+prices[j])){
-                        sumprice=money-(prices[i]+prices[j]);
-                    }
+                    sumprice=money-(prices[i]+prices[j]);
+                    
                 }
-            }
+            
         }
     }
     if(flag==true) return sumprice; return money;
