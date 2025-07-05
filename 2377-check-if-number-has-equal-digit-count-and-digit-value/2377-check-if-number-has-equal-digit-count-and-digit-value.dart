647aa408a89787b98e4579855d3471e8bf095e18
Muhammed Shebin .v <vakkayilshebin07@gmail.com>
2025-07-05 18:00:46 +0530

    Time: 1 ms (81.25%), Space: 149.5 MB (31.25%) - LeetHub

--- 2377-check-if-number-has-equal-digit-count-and-digit-value/2377-check-if-number-has-equal-digit-count-and-digit-value.dart
+++ 2377-check-if-number-has-equal-digit-count-and-digit-value/2377-check-if-number-has-equal-digit-count-and-digit-value.dart
@@ -1,11 +1,11 @@
 class Solution {
   bool digitCount(String num) {
-    for(int i =0;i< num.length;i++){
-        int count=0;
-        for(int j=0;j<num.length;j++){
-            if(i==int.parse(num[j]))count++;
+    for(int i = 0; i < num.length; i++){
+        int count = 0;
+        for(int j = 0; j < num.length; j++){
+            if(i == int.parse(num[j])) count++;
         }
-        if(count!=int.parse(num[i]))return false;
+        if(count != int.parse(num[i])) return false;
     }
     return true;
   }
